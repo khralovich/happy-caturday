739759ca90548f901fbb489f962828ecb19431f3
khralovich <chralovic@protonmail.com>
2021-06-07 01:35:04 +0200

    add: api url

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 const bgColorsArr = ['#0b223f', '#264653', '#2A9D8F', '#E9C46A', '#F4A261', '#E76F51', '#606C38', '#283618', '#BC6C25'];
 let bgColor;
+const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+
 
 function colorRandomizer() {
 bgColor = bgColorsArr[Math.floor(Math.random() * 8)];
