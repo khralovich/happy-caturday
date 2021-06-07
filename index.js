d21a2ffd8b909707696a2e82a75a1d690c30db77
khralovich <chralovic@protonmail.com>
2021-06-08 00:11:04 +0200

    feat: fetch functions

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 'use strict'
 
 
-const bgColorsArr = ['#0b223f', '#264653', '#2A9D8F', '#E9C46A', '#F4A261', '#E76F51', '#606C38', '#283618', '#BC6C25'];
-let bgColor;
+const bgColorsArr = ['#0b223f', '#264653', '#296b63', '#E9C46A', '#cc874e', '#E76F51', '#606C38', '#283618', '#BC6C25'];
 const apiUrl = 'https://api.thecatapi.com/v1/images/search';
-
+const catPhoto = document.querySelector('#kitty-img');
+const catQuote = document.querySelector('#quote');
+const author = document.querySelector('#author');
 
 function colorRandomizer() {
-bgColor = bgColorsArr[Math.floor(Math.random() * 8)];
+return document.body.style.backgroundColor  = bgColorsArr[Math.floor(Math.random() * 8)];
 }
+
+async function quoteRandomizer() {
+    const resp = await fetch('/quotes.json')
+  .then(resp => resp.json());
+  const randomNumber = Math.floor(Math.random() * 12);
+  console.log(Object.keys(resp)[randomNumber]);
+  console.log(Object.values(resp)[randomNumber]);
+}
+
+async function getACat(url) {
+    const response = await fetch(url);
+     let data = await response.json();
+     return catPhoto.src = data[0].url;
+}
+
+quoteRandomizer();
+colorRandomizer();
+getACat(apiUrl);
+
+
+
